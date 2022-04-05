79f19704ada97df43dfc8979c37dd94850e20676
Sviatoslav-Valentyn <Sviatoslav-Valentyn.Hvozdyk.IT.2021@lpnu.ua>
2022-04-05 14:56:32 +0300

    first

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PKR 3.2.2/Source.cpp"
+ 
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			const int i = 1;
+			Student s[i];
+
+			s[0].spec = CS;
+			s[0].physics = 4;
+			s[0].math = 4;
+			s[0].progr = 4;
+			double ExpValue = 4;
+			double ActValue = avgGrade(s, 0);
+
+			Assert::AreEqual(ExpValue, ActValue);
+		}
+	};
+}
